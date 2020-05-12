fedf8969bc993a3294253bbff49d3cba3e826424
Ingmar Jager <ingmarjager@gmail.com>
2020-05-12 15:52:21 +0200

    Add vl6180 and lis3dh i2c sensors to sensorlogger

--- sensorlogger/src/main.rs
+++ sensorlogger/src/main.rs
@@ -19,6 +19,8 @@ use stm32g0xx_hal::{
 };
 
 use dps422::{DPS422, self};
+use vl6180x::{VL6180X};
+use lis3dh::{Lis3dh, accelerometer::Accelerometer};
 
 use shared_bus;
 
@@ -33,6 +35,9 @@ fn main() -> ! {
     let mut led2 = gpiob.pb9.into_push_pull_output();
     led2.set_low().unwrap();
 
+    // Get the delay provider.
+    let cp = cortex_m::Peripherals::take().unwrap();
+    let mut delay = cp.SYST.delay(&mut rcc);
 
     let gpioa = dp.GPIOA.split(&mut rcc);
 
@@ -58,14 +63,22 @@ fn main() -> ! {
 
     let manager = shared_bus::CortexMBusManager::new(i2c);
 
-    let address = 0x76;
-    let mut dps = DPS422::new(manager.acquire(), address, &dps422::Config::new()).unwrap();
+    let mut dps = DPS422::new(manager.acquire(), 0x76, &dps422::Config::new()).unwrap();
 
-    writeln!(usart, "pressure sensor init done").unwrap();
+    writeln!(usart, "DPS422 init done").unwrap();
+
+    let mut vl6180x = VL6180X::new(manager.acquire()).unwrap();
+    writeln!(usart, "VL6180X init done..\n").unwrap();
+
+    let mut lis3dh = Lis3dh::new(manager.acquire(), 0x19).unwrap();
+    writeln!(usart, "Lis3dh init done..\n").unwrap();
+
+    lis3dh.set_range(lis3dh::Range::G8).unwrap();
+
     dps.trigger_measurement(true, true, false).unwrap();
+    vl6180x.start_ranging().unwrap();
 
     loop {
-        led2.toggle().unwrap();
 
         if dps.data_ready().unwrap() {
             led1.toggle().unwrap();
@@ -74,5 +87,20 @@ fn main() -> ! {
             writeln!(usart, "pressure: {:.1} [kPa]\t temp: {:.1} [˚C]", pressure, temp).unwrap();
             dps.trigger_measurement(true, true, false).unwrap();
         }
+
+        let status = vl6180x.int_status().unwrap();
+        if (status & 0b100) == 0b100 {
+            let range = vl6180x.read_range().unwrap();
+            writeln!(usart, "range = {} [mm]\n", range).unwrap();
+            vl6180x.clear_int().unwrap();
+            vl6180x.start_ranging().unwrap();
+            led2.toggle().unwrap();
+        }
+
+        let accel = lis3dh.acceleration().unwrap();
+        writeln!(usart, "accel = {}, {}, {}", accel.x, accel.y, accel.z).unwrap();
+
+        delay.delay_ms(500_u16);
+
     }
 }
